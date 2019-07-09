5d3b4366ec6c1263bd36f393e34fb600d060032a
Anton Shcherbakov <scherbakovx@Antons-MacBook-Pro.local>
2019-07-09 21:51:22 +0300

    Version 0.99

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <LandingPage msg="Здесь будет все про того самого Пиздабота (http://tele.click/pizda_da_bot), а пока подпишитесь на меня в Твиторе (https://twitter.com/scherbakovx) или напишите мне в Телегу (http://t.me/@scherbakovx)" />
-    <LandingPage msg2="Это, конечно, лучший сайт в Интернете, спасибо, что надонатили мне денег на pizda.bot!" />
+    <LandingPage msg="Здесь будет все про того самого Пиздабота (http://tele.click/pizda_da_bot)" />
+    <LandingPage msg2="Пока подпишитесь на меня в Твиторе (https://twitter.com/scherbakovx) или напишите мне в Телегу (http://t.me/@scherbakovx)" />
+    <LandingPage msg3="Это, конечно, лучший (НАИПИЗДАТЕЙШИЙ, ДА?) сайт в Интернете, спасибо, что надонатили мне денег на pizda.bot! (правда, спасибо)" />
+    <LandingPage msg4="<3 (люблю)" />
+    <LandingPage msg5="Конечно, этот сайт шутка, нормальный я сделаю ASAP, но вы сами знаете, какой из меня фронтендер (хуевый)" />
   </div>
 </template>
 
--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
-    <h1>{{ msg2 }}</h1>
+    <h2>{{ msg2 }}</h2>
+    <h3>{{ msg3 }}</h3>
+    <h4>{{ msg4 }}</h4>
+    <h5>{{ msg5 }}</h5>
   </div>
 </template>
 
@@ -10,7 +13,10 @@ export default {
   name: "LandingPage",
   props: {
     msg: String,
-    msg2: String
+    msg2: String,
+    msg3: String,
+    msg4: String,
+    msg5: String
   }
 };
 </script>
